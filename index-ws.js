daa56e14afc67306140e17f29527439c49ddd3c2
Ramdan Agus Saputra <ramdan@massivemusic.co.id>
2026-03-26 16:39:44 +0700

    add websocket feature

--- index-ws.js
+++ index-ws.js
@@ -13,3 +13,36 @@ server.on("request", app);
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+// Begin WebSockets
+const WebSocketServer = require("ws").Server;
+
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", (ws) => {
+  const numClients = wss.clients.size;
+  console.log(`Client connected: ${numClients}`);
+
+  if (ws.readyState === ws.OPEN) {
+    ws.send(`welcome! Current visitors: ${numClients}`);
+  }
+
+  ws.on("close", () => {
+    wss.broadcast(`Current visitors: ${wss.clients.size}`);
+    console.log("A client has disconnected");
+  });
+
+  ws.on("error", function error() {
+    console.log("An error occurred");
+  });
+});
+
+wss.broadcast = (data) => {
+  console.log("Broadcasting: ", data);
+
+  wss.clients.forEach((client) => {
+    if (client.readyState === client.OPEN) {
+      client.send(data);
+    }
+  });
+};
